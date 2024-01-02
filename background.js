ede3bd53f628cd648042d19f9433ee1d84659db6
Sanjay Sajwan <sanjaysajwan765@gmail.com>
2024-01-02 20:55:10 +0000

    added code for saving chrome history for a particular period

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
+let timeInterval=[];
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'getHistory') {
       const historyItems = [];
-      console.log("here")
+      console.log("here",message)
       const browsingHistory = chrome.history.getAllVisits();
   
       for (const visit of browsingHistory) {
@@ -25,43 +27,62 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   
       // Send a response to acknowledge the message
       sendResponse({ message: 'History saving started' });
+
+      timeInterval.push({startTime:new Date()})
   
       // Schedule a timer to send updates to the content script
-      const updateInterval = 1000; // Update every second
-      setInterval(() => {
-        const historyItems = [];
-        console.log("here")
-        const browsingHistory = chrome.history.getAllVisits();
+      // const updateInterval = 1000; // Update every second
+
+      // setInterval(() => {
+      //   const historyItems = [];
+      //   console.log("here",chrome.history)
+      //   const browsingHistory = chrome.history.getVisits();
+      //   for (const visit of browsingHistory) {
+      //       const item = {
+      //       url: visit.url,
+      //       title: visit.title,
+      //       date: new Date(visit.visitTime)
+      //       };
     
-        for (const visit of browsingHistory) {
-            const item = {
-            url: visit.url,
-            title: visit.title,
-            date: new Date(visit.visitTime)
-            };
-    
-            historyItems.push(item);
-        }
-        console.log("-->",historyItems)
-        chrome.runtime.sendMessage({ type: 'historyUpdated', historyItems: message.historyItems });
-      }, updateInterval);
+      //       historyItems.push(item);
+      //   }
+      //   console.log("-->",historyItems)
+      //   chrome.runtime.sendMessage({ type: 'historyUpdated', historyItems: message.historyItems });
+      // }, updateInterval);
     } else if (message.type === 'pauseSavingHistory') {
       // Pause history saving in the background script
       console.log('Received message to pause saving history');
-  
+      if(timeInterval.length>0){
+        timeInterval[timeInterval.length-1].endTime= new Date()
+      }
       // Send a response to acknowledge the message
       sendResponse({ message: 'History saving paused' });
   
       // Clear the update interval
-      clearInterval(updateInterval);
+      // clearInterval(updateInterval);
     } else if (message.type === 'stopSavingHistory') {
       // Stop history saving in the background script
-      console.log('Received message to stop saving history');
-  
+      console.log('Received message to stop saving history', timeInterval);
+      for(let i=0;i<timeInterval.length;i++){
+        let x=Math.floor(timeInterval[i].startTime.getTime())
+        let y=Math.floor(timeInterval[i].endTime.getTime())
+        console.log("x",x)
+        console.log("y",y)
+        chrome.history.search({
+          text:'',
+          startTime: x,
+          endTime: y
+        },
+        function(historyItems){
+          console.log("--->",historyItems)
+        }
+        )
+      }
+      
       // Send a response to acknowledge the message
       sendResponse({ message: 'History saving stopped' });
   
       // Clear the update interval
-      clearInterval(updateInterval);
+      // clearInterval(updateInterval);
     }
   });
--- script.js
+++ script.js
@@ -4,30 +4,31 @@ const stopButton = document.getElementById('stopButton');
 const historyList = document.getElementById('historyList');
 const historyItemsUl = document.getElementById('historyItems');
 
-const updateHistoryList = (historyItems) => {
-  historyItemsUl.innerHTML = '';
+// const updateHistoryList = (historyItems) => {
+//   historyItemsUl.innerHTML = '';
 
-  for (const item of historyItems) {
-    const historyItemLi = document.createElement('li');
-    const url = document.createElement('a');
-    url.href = item.url;
-    url.textContent = item.url;
+//   for (const item of historyItems) {
+//     const historyItemLi = document.createElement('li');
+//     const url = document.createElement('a');
+//     url.href = item.url;
+//     url.textContent = item.url;
 
-    const title = document.createElement('span');
-    title.textContent = ` (${item.title})`;
+//     const title = document.createElement('span');
+//     title.textContent = ` (${item.title})`;
 
-    const date = document.createElement('small');
-    date.textContent = ` - ${item.date.toLocaleString()}`;
+//     const date = document.createElement('small');
+//     date.textContent = ` - ${item.date.toLocaleString()}`;
 
-    historyItemLi.appendChild(url);
-    historyItemLi.appendChild(title);
-    historyItemLi.appendChild(date);
+//     historyItemLi.appendChild(url);
+//     historyItemLi.appendChild(title);
+//     historyItemLi.appendChild(date);
 
-    historyItemsUl.appendChild(historyItemLi);
-  }
-};
+//     historyItemsUl.appendChild(historyItemLi);
+//   }
+// };
 
 playButton.addEventListener('click', () => {
+  console.log("here")
   chrome.runtime.sendMessage({ type: 'startSavingHistory' });
 });
 
@@ -40,8 +41,8 @@ stopButton.addEventListener('click', () => {
   historyList.innerHTML = '';
 });
 
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === 'historyUpdated') {
-    updateHistoryList(message.historyItems);
-  }
-});
+// chrome.runtime.onMessage.addListener((message) => {
+//   if (message.type === 'historyUpdated') {
+//     updateHistoryList(message.historyItems);
+//   }
+// });
